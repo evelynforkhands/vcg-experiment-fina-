7f4c7a521af0a90eb6b3a806037f6cb67b9b6444
Daria Lyubaeva <lyubaevadaria@gmail.com>
2023-12-27 15:20:27 +0100

    refactor VCG pages with base class

--- group_decision/VCG/VCG_pages.py
+++ group_decision/VCG/VCG_pages.py
@@ -6,24 +6,25 @@ def is_displayed_VCG(self):
             return False
         return is_correct_treatment(self, 'VCG')
 
-class InfoVCG(PageWithTimeout):
-    form_model = 'player'
-    template_name = 'group_decision/VCG/InfoVCG.html'
-
+class VCGPage(PageWithTimeout):
+    @property
+    def template_name(self):
+        return f'group_decision/VCG/{self.__class__.__name__}.html'
     is_displayed = is_displayed_VCG
 
+
+
+class InfoVCG(VCGPage):
+    form_model = 'player'
     def vars_for_template(self):
         return {
             'order': self.group.round_number,
             'strings': C.strings,
         }
 
-class TestVCG_1(PageWithTimeout):
+class TestVCG_1(VCGPage):
     form_model = 'player'
     form_fields = ['vcg_comprehension_1']
-    template_name = 'group_decision/VCG/TestVCG_1.html'
-
-    is_displayed = is_displayed_VCG
 
     def error_message(self: Player, values):
         if values['vcg_comprehension_1'] == '2':
@@ -43,11 +44,9 @@ class TestVCG_1(PageWithTimeout):
 
 
 
-class TestVCG_2(PageWithTimeout):
+class TestVCG_2(VCGPage):
     form_model = 'player'
     form_fields = ['vcg_comprehension_2']
-    template_name = 'group_decision/VCG/TestVCG_2.html'
-    is_displayed = is_displayed_VCG
 
     def error_message(self, values):
         if values['vcg_comprehension_2'] == '2':
@@ -59,3 +58,14 @@ class TestVCG_2(PageWithTimeout):
         elif values['vcg_comprehension_2'] == '4':
             self.number_of_tries_vcg += 1
             return f"This answer is incorrect. Decisive influence means that if a tenant had not participated in the bidding, other tenants would have been matched to rooms they like more. "
+
+
+class DecisionVCG(VCGPage):
+    form_model = 'player'
+    form_fields = ['bid_room_A', 'bid_room_B', 'bid_room_C']
+
+
+    def vars_for_template(self):
+        return {
+            'strings': C.strings,
+        }
